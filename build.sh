6f0d9b953800359293e535b720bceda584ac2e4e
zyddnys <zyddnys@outlook.com>
2021-05-14 16:50:19 -0400

    no more docker build

--- build.sh
+++ build.sh
@@ -2,4 +2,4 @@ export PKG_CONFIG_ALLOW_CROSS=1
 export OPENSSL_STATIC=true
 export OPENSSL_DIR=/musl
 cargo build --target x86_64-unknown-linux-musl --release
-docker build -t pvgql .
+#docker build -t pvgql .
